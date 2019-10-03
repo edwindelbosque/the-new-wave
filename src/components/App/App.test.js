1fae698df7a3fceb5476a59983b1448931b4911d
Edwin Del Bosque <edwindelbosquer@gmail.com>
2019-10-03 11:01:37 -0600

    Add and pass more tests in App and SearchForm components

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,19 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
 import { shallow } from 'enzyme';
 
 describe('App', () => {
   it('should match the snapshot', () => {
-    const wrapper = shallow(
-      <App />
-    )
+    const wrapper = shallow(<App />)
+
     expect(wrapper).toMatchSnapshot();
   });
 
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
-    ReactDOM.unmountComponentAtNode(div);
-  });
+  it('should update state currentTopic when selectTopic is invoked', () => {
+    const wrapper = shallow(<App />);
+    const mockSelectTopic = 'local'
+    const expected = 'local';
+
+    wrapper.instance().selectNews(mockSelectTopic)
+    expect(wrapper.state('currentTopic')).toEqual(expected);
+  })
 })
--- src/components/SearchForm/SearchForm.test.js
+++ src/components/SearchForm/SearchForm.test.js
@@ -3,13 +3,25 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 describe('SearchForm', () => {
+  const searchNews = jest.fn();
 
   it('should match the snapshot information passed correctly', () => {
     const wrapper = shallow(
       <SearchForm
-        searchNews={jest.fn()}
+        searchNews={searchNews}
       />
     )
     expect(wrapper).toMatchSnapshot();
   })
+
+  it('should call the method prop when onChange', () => {
+    const wrapper = shallow(
+      <SearchForm
+        searchNews={searchNews}
+      />
+    )
+    wrapper.find('input').simulate('change', { target: { value: 'A' } });
+    expect(searchNews).toHaveBeenCalled();
+  })
+
 })
